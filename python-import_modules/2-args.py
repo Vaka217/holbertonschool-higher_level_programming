fc674af0d3b12b0498db91d68bc4fd9b5a3cc478
root <gonzalocabaleiro12@gmail.com>
2023-01-16 12:48:21 -0800

    Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv)
+    if args == 1:
+        print("{} argument.".format(args - 1))
+    elif args == 2:
+        print("{} argument:".format(args - 1))
+    else:
+        print("{} arguments:".format(args - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
