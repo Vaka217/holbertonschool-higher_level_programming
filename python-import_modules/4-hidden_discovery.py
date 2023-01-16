4cba0558def495343acc9131ebbdfcfff8b8e289
root <gonzalocabaleiro12@gmail.com>
2023-01-16 13:37:53 -0800

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    namelist = dir(hidden_4)
-    i = 0
-    while l:
-        print(namelist[i])
-        i += 1
+    import hidden_4
+    names_list = dir(hidden_4)
+    for names in names_list:
+        if (names[:2] != '__'):
+            print(names)
