b32ea75347188ed910029175e82ddf2d7a66e1dc
root <gonzalocabaleiro12@gmail.com>
2023-01-17 08:07:53 -0800

    Task 6

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    import calculator_1
+    
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    
+    a, b, operator = int(argv[1]), int(argv[3]), argv[2]
+    if operator == '+' :
+        result = a + b
+    elif operator == '-':
+        result = a - b
+    elif operator == '*':
+        result = a * b
+    elif operator == '/':
+        result = a / b
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+        
+    print(a, operator, b, '=', result)
+ 
