1aaf8f14a52b6bd80dfaa104061792f4b2bbd6dd
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-01-23 05:38:03 -0800

    Fixes task 1-2

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if len(my_list) > idx or idx < 0:
+    if len(my_list) < idx or idx < 0:
         return None
-    print("Element at index {} is {}".format(index, my_list(index)))
+    return my_list[idx]
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,4 +2,5 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or len(my_list) < idx:
         return my_list
-    return my_list.insert(idx, element)
+    my_list.insert(idx, element)
+    return my_list
