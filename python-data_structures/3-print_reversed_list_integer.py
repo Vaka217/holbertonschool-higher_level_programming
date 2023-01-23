8a5867a12d6884487107f58412e3c84b8eca4214
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-01-23 05:50:18 -0800

    Handle none case

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) > 0:
-        my_list.reverse()
+    if my_list == None:
+        return    
+    my_list.reverse()
     for element in my_list:
         print("{:d}".format(element))
