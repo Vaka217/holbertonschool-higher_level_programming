e089eed1cdc8f29b58b469439373e6dcfe60e714
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-01-23 06:11:49 -0800

    Task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    cp_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            cp_str += my_string[i]
+    return cp_str
