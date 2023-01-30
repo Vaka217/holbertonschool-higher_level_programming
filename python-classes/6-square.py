cababdfd053ba31c86f85a77a1d73b59f9da97e1
root <gonzalocabaleiro12@gmail.com>
2023-01-30 09:57:24 -0800

    Task 6 fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,6 +21,8 @@ class Square:
         """Prints in stdout the square with the character #"""
         if self.__size == 0:
             print("")
+        for h in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
             for z in range(self.__position[0]):
                 print(" ", end="")
@@ -51,7 +53,7 @@ class Square:
     @position.getter
     def position(self, position):
         """ position setter"""
-        if isinstance(position, tuple) is False or x in position < 0:
+        if not isinstance(position, tuple) or all(x >= 0 for x in position):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
