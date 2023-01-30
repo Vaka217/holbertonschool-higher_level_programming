c5ecd326b12c92e309e8fc4332427bfd01780da2
root <gonzalocabaleiro12@gmail.com>
2023-01-30 07:55:09 -0800

    Fix isinstance arguments

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
         __size (int): Size of the square
     """
     def __init__(self, size=0):
-        if type(size) != int:
+        if isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
