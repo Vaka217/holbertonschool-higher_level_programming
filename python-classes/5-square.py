01cbdd5e28845613121069a3d19361ac25c2c1f2
root <gonzalocabaleiro12@gmail.com>
2023-01-30 09:24:04 -0800

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square by size and validates size
+    Attributes:
+        __size (int): Size of the square
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """Calculates area of a square
+        """
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+
+    @property
+    def size(self):
+        """ size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ size setter"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
