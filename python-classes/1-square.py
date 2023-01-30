7677eacae32c34cc06c3ff85875f92ddfb9ef220
root <gonzalocabaleiro12@gmail.com>
2023-01-30 07:52:29 -0800

    Task 0-3

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 
 class Square:
     """Defines a square by size
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
     """Defines a square by size and validates size
     Attributes:
