44ce5562afbbb4f4d4dfea3adf3abf55600f2542
root <gonzalocabaleiro12@gmail.com>
2023-01-31 04:37:35 -0800

    Advanced 1 comments

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""
+    Singly linked list module
+"""
+
 
 class Node:
+    """ Node"""
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Data getter"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Data setter"""
         if isinstance(value, int) is False:
             raise TypeError("data must be an integer")
         else:
@@ -18,10 +25,12 @@ class Node:
 
     @property
     def next_node(self):
+        """next_node getter"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """next_node setter"""
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         else:
@@ -29,10 +38,12 @@ class Node:
 
 
 class SinglyLinkedList:
+    """Singly linked list"""
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
+        """inserts a new Node into the correct sorted position in the list"""
         new = Node(value)
         if self.__head is None:
             new.next_node = None
@@ -48,6 +59,7 @@ class SinglyLinkedList:
             tmp.next_node = new
 
     def __str__(self):
+        """__str__ method"""
         linked_list = []
         tmp = self.__head
         while tmp.next_node is not None:
