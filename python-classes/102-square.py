d73d9936a118158b27041847faaf23730a5ba612
root <gonzalocabaleiro12@gmail.com>
2023-01-31 05:26:10 -0800

    Advanced 3

---
+++ python-classes/102-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square by size and validates size
+    Attributes:
+        __size (int): Size of the square
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """Calculates area of a square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ size setter"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def __eq__(self, other):
+        """ equal method"""
+        if self.area() == other.area():
+            return True
+        return False
+
+    def __lt__(self, other):
+        """ lesser method"""
+        if self.area() < other.area():
+            return True
+        return False
+
+    def __le__(self, other):
+        """ equal or less method"""
+        if self.area() == other.area() or self.area() < other.area():
+            return True
+        return False
