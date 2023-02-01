d744b13f7c4d29e6f9c82908b936813d99ec626d
root <gonzalocabaleiro12@gmail.com>
2023-02-01 05:58:23 -0800

    Task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in (".", "?", ":"):
+            print("\n")
+            i += 1
+        i += 1
