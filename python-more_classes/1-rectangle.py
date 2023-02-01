af1b9929926bf39ea849fedc209a56c4d6c50690
root <gonzalocabaleiro12@gmail.com>
2023-02-01 08:52:00 -0800

    Task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """
+    Defines a rectangle by width and height
+    """
+    def __init__(self, width=0, height=0):
+        """
+        init method
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
