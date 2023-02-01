6869452010f44314f1486a6c225a9f427b439bc9
root <gonzalocabaleiro12@gmail.com>
2023-02-01 09:26:31 -0800

    Added getters and setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
         """
         init method
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
