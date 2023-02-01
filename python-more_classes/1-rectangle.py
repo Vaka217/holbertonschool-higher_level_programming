755df0367d2bfad9ec9cda2dbd674cc85180f32f
root <gonzalocabaleiro12@gmail.com>
2023-02-01 09:22:31 -0800

    Added getters and setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,13 +10,33 @@ class Rectangle:
         """
         init method
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
+
+    @property
+    def width(self):
+        """ getter width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """ getter height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ setter width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ setter height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
