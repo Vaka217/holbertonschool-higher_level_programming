ac9d3e4009a7c0db59ac0966352c242478b6f4aa
root <gonzalocabaleiro12@gmail.com>
2023-02-01 10:57:43 -0800

    Task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area < rect_2.area:
+        if rect_1.area() < rect_2.area():
             return rect_2
         return rect_1
 
