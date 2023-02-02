d70b299cc7686945aae82a8207a735c9a57d9b17
root <gonzalocabaleiro12@gmail.com>
2023-02-02 05:02:58 -0800

    Task10

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import dis
+import math
+
+
+class MagicClass:
+    """ Class with methods to calculate area and circumference of a circle"""
+    def __init__(self, radius=0):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """ Circle area"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """ Circle circumference"""
+        return 2 * math.pi * self.__radius
+
+
+dis.dis(MagicClass)
