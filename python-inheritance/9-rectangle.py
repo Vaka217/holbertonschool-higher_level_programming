efdf8ebb94c43d9866bcbfee3d4f14538a319451
root <gonzalocabaleiro12@gmail.com>
2023-02-06 11:43:33 -0800

    Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a class Rectangle"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", width)
+        self.__width = width
+
+    def print(self):
+        """Prints width and area of the given rectangle"""
+        print(f'[Rectangle] {self.__width}/{self.__height}')
+
+    def __str__(self):
+        """The informal representation of the rectangle"""
+        return f'[Rectangle] {self.__width}/{self.__height}'
+
+    def area(self):
+        """Returns the area of a rectangle"""
+        return self.__height * self.__width
