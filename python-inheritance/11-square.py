dd6d7f16378d4737ac6707966de74b8d472d7bb7
root <gonzalocabaleiro12@gmail.com>
2023-02-06 11:54:27 -0800

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Method with the class Square"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square that defines a square"""
+    def __init__(self, size):
+        BaseGeometry.integer_validator(self, "size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f'[Square] {self.__size}/{self.__size}'
