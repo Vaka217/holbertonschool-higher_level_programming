1c51cd8c129abfe02935d97166bba26aae5813af
root <gonzalocabaleiro12@gmail.com>
2023-02-06 10:23:07 -0800

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+    def area(self):
+        """Public instance method that raises an Exception with a message"""
+        raise Exception("area() is not implemented")
