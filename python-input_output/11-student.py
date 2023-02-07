a355c3a3fbf3185acc26d44f4052d1ed24701896
root <gonzalocabaleiro12@gmail.com>
2023-02-07 09:45:38 -0800

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" 11-student module"""
+
+
+class Student:
+    """ Class Student that defines a student by name, last name and age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance"""
+        self.first_name = json.get('first_name')
+        self.last_name = json.get('last_name')
+        self.age = json.get('age')
