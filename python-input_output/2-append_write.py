d73da8c79cb6e036964946e5e93f1193589ca203
root <gonzalocabaleiro12@gmail.com>
2023-02-07 04:48:08 -0800

    Task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to append a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """ Append a string to a text file (UTF8) and returns the number of
+    characters written"""
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
