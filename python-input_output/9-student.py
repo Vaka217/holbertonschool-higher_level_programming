ba891817b42e2e9fc2c424889d5ab09c03c9c567
root <gonzalocabaleiro12@gmail.com>
2023-02-07 09:09:35 -0800

    Task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 9-student module"""
+
+
+class Student:
+    """ Class Student that defines a student by name, last name and age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
