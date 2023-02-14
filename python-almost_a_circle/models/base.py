b22752e4b688911c55e5dc4a2343837aef12d611
root <gonzalocabaleiro12@gmail.com>
2023-02-14 03:05:54 -0800

    Task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,8 +3,12 @@
 
 
 class Base:
+    """ Class that works as the base of all the other classes"""
     __nb_objects = 0
-    def __init__(self, id=None):
-        ++Base.__nb_objects
-        self.id = Base.__nb_objects
 
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
