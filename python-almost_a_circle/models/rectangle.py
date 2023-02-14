e51bfc9e3f7f49a40ee12e92c2c387c31fe0ab92
root <gonzalocabaleiro12@gmail.com>
2023-02-14 04:28:35 -0800

    Task 8 and 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -106,3 +106,20 @@ class Rectangle(Base):
         """ __str__ method"""
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} "
                 f"- {self.__width}/{self.__height}")
+    
+    def update(self, *args, **kwargs):
+        """ assigns an argument to each attribute"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
