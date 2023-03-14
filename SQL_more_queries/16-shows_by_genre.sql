a43e18516393b40f8f941af801d151b0ef492c18
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-03-14 10:11:52 -0700

    Fix keywords uppercase in task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 SELECT sh.title, g.name
 FROM tv_shows sh
-    LEFT JOIN tv_show_genres as tsg ON sh.id = tsg.show_id
-    LEFT JOIN tv_genres as g ON tsg.genre_id = g.id
+    LEFT JOIN tv_show_genres AS tsg ON sh.id = tsg.show_id
+    LEFT JOIN tv_genres AS g ON tsg.genre_id = g.id
 ORDER BY title, name;
