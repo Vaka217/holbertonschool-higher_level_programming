58f1594fb4b9469acbd3881c381529e4eb441249
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-03-14 13:45:52 -0700

    Task 4

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE SUBSTRING(name, 1, 1) = 'N'")
-    [print(row) for row in cur.fetchall()]
+    cur.execute("SELECT * FROM states ORDER BY id")
+    [print(row) for row in cur.fetchall() if row[1][0] == 'N']
