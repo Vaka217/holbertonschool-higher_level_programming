398a9e55be54b4e6917970398072f9f73bb0a6cc
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-03-14 12:13:27 -0700

    Tasks 1-3

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 2-my_filter_states module"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+                FROM states WHERE BINARY name ='{}'".format(argv[4]))
+    [print(row) for row in cur.fetchall()]
