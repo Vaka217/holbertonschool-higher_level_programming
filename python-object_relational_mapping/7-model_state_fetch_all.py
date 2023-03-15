957f2bb552b89da52c6528f14e085f7f61b9f792
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-03-15 11:25:44 -0700

    Task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
+from sqlalchemy import (create_engine)
 from model_state import Base, State
 
 
-from sqlalchemy import (create_engine)
-from sqlalchemy import inspect
-
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-connection = engine.connect() 
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                       (sys.argv[1], sys.argv[2], sys.argv[3]),
+                       pool_pre_ping=True)
+connection = engine.connect()
 result = connection.execute('SELECT * FROM states')
 [print(f"{elem[0]}: {elem[1]}") for elem in result]
