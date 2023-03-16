24434a9928a1dbc9ebc9980adb794263e5bfa27f
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-03-16 05:59:12 -0700

    Task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.query(State).filter(State.name.contains("a")).delete(synchronize_session=False)
+    session.query(State).filter(State.name.contains("a"))\
+        .delete(synchronize_session=False)
     session.commit()
