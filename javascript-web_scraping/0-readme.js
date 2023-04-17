767368ccfd733175efd52cc6cbec01049bea083f
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-04-17 06:05:10 -0700

    Task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+const fs = require('fs');
+fs.readFile(args[0], 'utf-8', (error, data) => {
+    if (error) throw error;
+      console.log(data.toString());
+  });
