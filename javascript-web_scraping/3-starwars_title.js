55da3dcabb680b8c853cddc19000659b04b8421e
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-04-17 07:45:55 -0700

    Task 3 and 4

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${args[0]}`;
-console.log(url);
-request(url, (error, response, body) => {
+const url = `https://swapi-api.alx-tools.com/api/films/${args[0]}`;
+request(url, function (error, response, body) {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-  console.log(response.title);
 });
---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+request(args[0], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+    for (const i in results) {
+      if (results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
